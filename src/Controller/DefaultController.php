c2e061768a77408491f87f282fe203629741d148
Leandro Marchon <marchonleandro@gmail.com>
2018-11-30 09:36:53 -0200

    Configurando o ambiente de desenvolvimento.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     * @Template("default/index.html.twig")
+     */
+    public function index()
+    {
+        return [];
+    }
+}
